c7583adcac88f34187221790f84b525bfbc73cc5
Eyad <eyadjob@yahoo.com>
2020-11-03 11:29:03 +0200

    sta

--- src/main/java/facebookposition/fbquestions/MinimumLengthSubstrings.java
+++ src/main/java/facebookposition/fbquestions/MinimumLengthSubstrings.java
@@ -13,8 +13,7 @@ public class MinimumLengthSubstrings {
         String s2 = "bfbeadbcbcbfeaaeefcddcccbbbfaaafdbebedddf";
         String t2 = "cbccfafebccdccebdd";
         System.out.println(minLengthSubstring2(s2, t2));
-
-
+        
     }
 
     public static int minLengthSubstring2(String s, String t) {
@@ -34,8 +33,5 @@ public class MinimumLengthSubstrings {
         }
         return max + 1;
     }
-
-
-
 
 }
