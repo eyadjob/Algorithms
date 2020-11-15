7059fde1239fe09cc27e4d46a88ca8e18f0f2b51
Eyad Mubarak <e.mubarak@iyelo.com>
2020-11-15 13:18:02 +0200

    new changes

---
+++ src/main/java/facebookposition/fbquestions/datastructure/AddAndSearchWords.java
@@ -0,0 +1,32 @@
+package facebookposition.fbquestions.datastructure;
+
+public class AddAndSearchWords {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class TrieNode{
+
+
+    }
+
+public class WordDictionary {
+
+
+        public void addWord(String word) {
+
+        }
+
+        public boolean searchWordInNod(String word,TrieNode) {
+
+        }
+
+        public boolean searchWord(String word) {
+
+        }
+
+}
+
+
+}
