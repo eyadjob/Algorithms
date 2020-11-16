a4bac2febdff51f35e3141f76f1f980e46a16573
e.mubarak <e.mubarak@alwefaq.com>
2020-11-16 18:42:17 +0200

    new

--- src/main/java/facebookposition/fbquestions/linkedlist/ReverseOperations.java
+++ src/main/java/facebookposition/fbquestions/linkedlist/ReverseOperations.java
@@ -3,14 +3,22 @@ package facebookposition.fbquestions.linkedlist;
 public class ReverseOperations {
 
     public static void main(String[] args) {
-        int[] list = {1, 2, 8, 9, 12, 16};
         ReverseOperations reverseOperations = new ReverseOperations();
+
+        int[] arr_2 = {2, 18, 24, 3, 5, 7, 9, 6, 12};
+//        int[] expected2 = {24, 18, 2, 3, 5, 7, 9, 12, 6};
+        reverseOperations.test(arr_2);
+
+        System.out.println();
+
+        int[] list = {1, 2, 8, 9, 12, 16};
         reverseOperations.test(list);
+
 
     }
 
     public Node reverse(Node head) {
-        Node node = head, prev = null;
+        Node node = head, oddStartNode = null, evenEnd = null;
         Node evenStart = null;
         boolean firstEvenNodeMarked = false, oddEnd = false;
         while (node != null) {
@@ -21,47 +29,60 @@ public class ReverseOperations {
                     firstEvenNodeMarked = true;
                 }
                 if (node.next == null) {
-                    reverseNodes(evenStart, prev);
+                    if ( evenEnd == evenStart) evenEnd = evenStart.next;
+                    reverseNodes(oddStartNode,evenStart, evenEnd,evenEnd.next);
                     firstEvenNodeMarked = false;
+                    node = node.next;
                 }
             } else {
+
                 if (oddEnd) {
-                    reverseNodes(evenStart, prev);
+                    reverseNodes(oddStartNode,evenStart, evenEnd,evenEnd.next);
+                    if (oddStartNode == null ) head = evenEnd;
                     firstEvenNodeMarked = false;
                     oddEnd = false;
+                    oddStartNode = node;
+                } else {
+                    oddStartNode = node;
                 }
             }
-            prev = node;
+            evenEnd = node;
             node = node.next;
+
         }
         return head;
     }
 
-    public void reverseNodes(Node head, Node end) {
-        Node node = head;
-        while (node != null && node != end) {
-            Node temp = node.next;
-            temp.next = node;
-            node = node.next;
+    public void reverseNodes(Node oddStart, Node evenStart, Node evenEnd, Node oddEnd) {
+        Node node = evenStart;
+        Node prev = null;
+        while (node != null && node != oddEnd) {
+            Node next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
         }
-      
+        if ( oddStart!= null) oddStart.next = evenEnd;
+        evenStart.next = oddEnd;
     }
 
     public void test(int[] arr) {
         Node node = fillNodes(arr);
-        reverse(node);
+        node =reverse(node);
         printNodes(node);
     }
 
     public Node fillNodes(int[] arr) {
         Node head = new Node(0);
         Node node = head;
-        for (int v : arr) {
-            node.data = v;
-            node.next = new Node(0);
-            node = node.next;
+        for (int i =0; i < arr.length; i++) {
+            node.data = arr[i];
+            if ( i != arr.length-1) {
+                node.next = new Node(0);
+                node = node.next;
+            }
         }
-        node.next = null;
+        node = null;
         return head;
     }
 
