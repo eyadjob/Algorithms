3a117a42297a2b4d3a798fc1055359a53a930d7b
Eyad Mubarak <e.mubarak@iyelo.com>
2020-12-02 17:44:24 +0200

    newchange

--- src/main/java/facebookposition/fortifying/StringToInteger.java
+++ src/main/java/facebookposition/fortifying/StringToInteger.java
@@ -3,13 +3,115 @@ package facebookposition.fortifying;
 public class StringToInteger {
     public static void main(String[] args) {
 
+
+        String s11 = "-5-";
+        System.out.println(myAtoi(s11));
+
+
+        String s10 = "   +0 123";
+        System.out.println(myAtoi(s10));
+
+
+        String s9 = "  -0012a42";
+        System.out.println(myAtoi(s9));
+
+        String s8 = " ";
+        System.out.println(myAtoi(s8));
+
+        String s = "   -42";
+        System.out.println(myAtoi(s));
+
+        String s2 = "4193 with words";
+        System.out.println(myAtoi(s2));
+
+        String s3 = "words and 987";
+        System.out.println(myAtoi(s3));
+
+        String s4 = "-91283472332";
+        System.out.println(myAtoi(s4));
+
+        String s5 = "+-12";
+        System.out.println(myAtoi(s5));
+
+        String s6 = "+1";
+        System.out.println(myAtoi(s6));
+
+        String s7 = "00000-42a1234";
+        System.out.println(myAtoi(s7));
+    }
+
+    public static int myAtoi2(String s) {
+
+        if (s.length() == 0) return 0;
+        StringBuilder sb = new StringBuilder();
+        boolean flag = false, whiteSpaceFoundFinished = false, numberFound = false, signWasFound = false;
+        for (char c : s.toCharArray()) {
+            if (Character.isWhitespace(c)) {
+                continue;
+            }
+            whiteSpaceFoundFinished = true;
+            if (!numberFound && !signWasFound && c != '-' || c != '+') sb.append(c);
+            else
+                break;
+            signWasFound = true;
+            if (!Character.isDigit(c)) {
+                if (!numberFound)
+                    continue;
+                else break;
+            }
+            if (Character.isDigit(c)) {
+                numberFound = true;
+                sb.append(c);
+                continue;
+            }
+
+
+        }
+        return num;
     }
 
     public static int myAtoi(String s) {
+        if (s.length() == 0) return 0;
+        StringBuilder sb = new StringBuilder();
+        boolean flag = false, whiteSpaceFoundFinished = false, numberFound = false, signWasFound = false;
+        for (char c : s.toCharArray()) {
+            if (!whiteSpaceFoundFinished && Character.isWhitespace(c)) {
+                continue;
+            }
+            whiteSpaceFoundFinished = true;
+            if (!Character.isDigit(c) && !flag && c != '-' && c != '+') return 0;
+            if (Character.isDigit(c) || c == '-' || c == '+') {
+                if (numberFound && signWasFound && (c == '-' || c == '+')) break;
+                if (c == '-' || c == '+') signWasFound = true;
+                if (Character.isDigit(c)) numberFound = true;
+                sb.append(c);
+                flag = true;
+                continue;
+            }
+            if (!Character.isDigit(c) && whiteSpaceFoundFinished) {
+                if (!numberFound) return 0;
+                if (sb.length() == 1 && (sb.charAt(0) == '-' || sb.charAt(0) == '+')) return 0;
+                long re = 0l;
+                try {
+                    re = Long.parseLong(sb.toString());
+                } catch (Exception e) {
+                    return 2147483647;
+                }
+                return re < 0 ? (int) Math.max(re, Integer.MIN_VALUE) : (int) Math.min(re, Integer.MAX_VALUE);
+            }
 
+        }
+        if (sb.length() == 1 && (sb.charAt(0) == '-' || sb.charAt(0) == '+')) return 0;
+        if (!numberFound) return 0;
+        long re = 0l;
+        try {
+            re = Long.parseLong(sb.toString());
+        } catch (Exception e) {
+            return 2147483647;
+        }
+        return re < 0 ? (int) Math.max(re, Integer.MIN_VALUE) : (int) Math.min(re, Integer.MAX_VALUE);
     }
 }
-
 
 
 //    Implement atoi which converts a string to an integer.
